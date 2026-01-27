949a94bf2a02672ba08a95623716c05d6b3787d4
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-01-27 14:18:15 -0800

    new if statement on js

--- pizza.js
+++ pizza.js
@@ -1,1 +1,50 @@
-alert("hello");
+
+//option to write annonymous function
+// document.getElementById("pizza-form").onsubmit = () => {
+// function validate(){
+//     alert("hello");
+// }
+
+
+document.getElementById("pizza-form").onsubmit = validate;
+function validate(){
+
+    clearErrors();
+
+    let isValid = true;
+
+     // first name validation
+    let fname = document.getElementById("fname").value.trim();
+    if(!fname){
+        document.getElementById("err-fname").style.display="block";
+        isValid =false;
+    }
+    //last name validation
+      let lname = document.getElementById("lname").value.trim();
+    if(!lname){
+        document.getElementById("err-lname").style.display="block";
+        isValid =false;
+    }
+    //email validation
+       let email = document.getElementById("email").value.trim();
+    if(!email){
+        document.getElementById("err-email").style.display="block";
+        isValid =false;
+    }
+    return isValid;
+
+}
+
+function clearErrors(){
+    let errors = document.getElementsByClassName("err");
+    for(let i=0; i<errors.length; i++){
+        errors[i].style.display="none";
+    }
+}
+
+
+
+
+
+
+
