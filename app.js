bf9e83c1e446a4129bd3a56b6ab750096145d0e7
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-03-03 21:07:07 -0800

    added env.

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mysql2 from 'mysql2';
+import dotenv from 'dotenv';
+
+//load environment variables from .env file
+dotenv.config();
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
+//console.log(process.env.DB_HOST);
+
 const app = express();
 const PORT = 3000;
 app.use(express.static('public'));
@@ -10,8 +21,31 @@ app.set('view engine', 'ejs');
 // form data and store it in req.body
 app.use(express.urlencoded({ extended: true }));
 
-// Create a temp array to store orders
-const orders = []; 
+
+//create a pool bucket of database connections
+const pool = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+    
+}).promise();
+
+
+// database test route
+//async function tells js to wait for the database query to finish before moving on
+app.get('/db-test', async(req, res) => {
+    try {
+        const pizza_orders = await pool.query('SELECT * FROM orders');
+        res.send(pizza_orders[0]);
+    } catch(err) {
+        console.error('Database error: ', err);
+        res.status(500).send(err.message);
+     }
+
+});
+
 
 // Default route
 app.get('/', (req, res) => {
@@ -29,30 +63,44 @@ app.get('/thank-you', (req, res) => {
 });
 
 // Admin route
-app.get('/admin', (req, res) => {
-    res.render('admin', { orders });
+app.get('/admin', async(req, res) => {
+
+    //read all orders from the database
+    //read newest first
+    let sql = 'SELECT * FROM orders ORDER BY timestamp DESC';
+    const orders = await pool.query(sql);
+    console.log(orders);
+
+    res.render('admin', { orders: orders[0] });
 });
 
 // Submit order route
 // {"fname":"a","lname":"aa","email":"a",
 // "method":"delivery","toppings":["artichokes"],
 // "size":"small","comment":"","discount":"on"}
-app.post('/submit-order', (req, res) => {
-    
-    // Create a JSON object to store the order data
-    const order = {
-        fname: req.body.fname,
-        lname: req.body.lname,
-        email: req.body.email,
-        method: req.body.method,
-        toppings: req.body.toppings ? req.body.toppings : "none",
-        size: req.body.size,
-        comment: req.body.comment,
-        timestamp: new Date()
-    };
+app.post('/submit-order', async(req, res) => {
 
-    // Add order object to orders array
-    orders.push(order);
+    const order = req.body;
+    
+    // Create an array of order data
+    const params = [
+       req.body.fname,
+        req.body.lname,
+        req.body.email,
+        req.body.size,
+        req.body.method,
+        Array.isArray(req.body.toppings) ? req.body.toppings.join(", ") : "none"
+];
+
+
+    // insert new order to the database
+      const sql = `INSERT INTO orders (fname,       lname, email, size, method, toppings)
+                 VALUES (?, ?, ?, ?, ?, ?)`;
+
+    const result = await pool.execute(sql,params);
+    console.log("Order inserted with ID: ", result[0].insertId);
+
+
     res.render('confirmation', { order });
 });
 
